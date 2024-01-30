9bccc1ff6abb1c7b5d35d3719aa80819b7e700b2
Donovan Vernon <44986610+dvernon5@users.noreply.github.com>
2024-01-30 11:35:00 -0800

    Create StudentManagementSystem.h Declare the Constructor to launch the Student Management System

---
+++ StudentManagementSystem.h
@@ -0,0 +1,12 @@
+#ifndef StudentManagementSystem_h
+#define StudentManagementSystem_h
+#include "LaunchManager.h"
+
+class StudentManagementSystem {
+  public:
+    StudentManagementSystem();
+  
+  private:
+    LaunchManager launch;
+};
+#endif /* StudentManagementSystem_h */
